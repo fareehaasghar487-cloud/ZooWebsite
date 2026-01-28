f453459d46d8fc5f418ffc8c57d7f604bf5e483a
fareehaasghar487-cloud <fareehaasghar487@gmail.com>
2026-01-28 12:43:21 +0500

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App"; 
 import "./index.css";
-import { AuthProvider } from "./context/UserProfile"; 
+import { AuthProvider } from "./context/userProfile"; 
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css"; 
 createRoot(document.getElementById("root")).render( 
